8997089e4ee4bb45454f65f1b9366dbbbf37608c
Régis Berthelot <regis.berthelot@epitech.eu>
2017-06-10 11:34:18 +0200

    Minor enhancement of the code
    Reduced number of lines by lowering the use of `if` keyword ;
    Forced the deletion of temporary .tar file no matter the return value ;
    Limited the length of line to 80 characters max.

--- tar7z.sh
+++ tar7z.sh
@@ -5,31 +5,25 @@
 ## Login   <regis.berthelot@epitech.eu>
 ## 
 ## Started on  Sat Jun  3 07:35:03 2017 Régis Berthelot
-## Last update Sun Jun  4 10:15:50 2017 Régis Berthelot
+## Last update Sat Jun 10 11:33:34 2017 Régis Berthelot
 ##
 #
 
 TMPNAME="/tmp/autoscript_archive.tar"
 DATE=`date -Iminutes`
-RC=0
 
 if test "$1"
 then
-    tar cfv "$TMPNAME" "$@"
-    RC=$?
-    if test "$RC" -eq 0
+    tar cfv "$TMPNAME" "$@" && 7z a "$DATE".tar.7z "$TMPNAME"
+    if test "$?" -eq 0
     then
-	7z a "$DATE".tar.7z "$TMPNAME"
-	RC=$?
-	rm "$TMPNAME"
-	if test "$RC" -eq 0
+	echo "\nDo you want to rename the archive? (DO NOT add extension)"
+	echo "Press Enter to leave default name\nDefault name: $DATE.tar.7z"
+	read NEWNAME
+	if test "$NEWNAME"
 	then
-	    echo "\nDo you want to rename the archive (extension will be appended automatically)?\nPress Enter to leave default name\nDefault name: $DATE.tar.7z"
-	    read NEWNAME
-	    if test "$NEWNAME"
-	       then
-		   mv "$DATE".tar.7z "$NEWNAME".tar.7z
-	    fi
+	    mv "$DATE".tar.7z "$NEWNAME".tar.7z
 	fi
     fi
+    rm -f "$TMPNAME"
 fi
